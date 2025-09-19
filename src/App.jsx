c9be7a0c50d1e2daafaea138c3f33d0d3e38b11a
pipetoroc <ftorocastilla@gmail.com>
2025-09-19 18:24:47 +0100

    resolve exercise 5.1

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async event => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+
+    if (user === null){
+     return (
+      <div>
+        <h2>Log in to application</h2>
+         <form onSubmit={handleLogin}>
+           <div>
+             <label>
+               username
+               <input
+                 type="text"
+                 value={username}
+                 onChange={({ target }) => setUsername(target.value)} />
+             </label>
+           </div>
+           <div>
+             <label>
+               password
+               <input
+                 type="text"
+                 value={password}
+                 onChange={({ target }) => setPassword(target.value)} />
+             </label>
+           </div>
+           <button type='submit'>login</button>
+         </form>
+       </div>
+     ) 
+    }
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog => <Blog key={blog.id} blog={blog}/>)}
+    </div>
+  )
+}
+
+export default App
