143f346138a16a0ecb77da7590f1eea6aaab1bc6
pipetoroc <ftorocastilla@gmail.com>
2025-09-23 17:59:09 +0100

    exercise 5.7 add button to show or hide details in blogs

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,8 +1,28 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-    <button>view</button>
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  console.log(blog)
+
+  const toggleDetails = () => {
+    setShowDetails(details => !details)
+  }
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}
+      <button onClick={toggleDetails}>
+        {showDetails ? "Hide" : "View"}
+      </button>
+
+      {showDetails && (
+        <div >
+          <p>Url: {blog.url}</p>
+          <p>Likes: {blog.likes}</p>
+          <p>Author: {blog.author}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
