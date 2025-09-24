77dd46dc367b673ac26de83e974fbdd4e68df3e9
pipetoroc <ftorocastilla@gmail.com>
2025-09-24 18:55:50 +0100

    exercise 5.8 implementation put request in frontend

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react"
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [showDetails, setShowDetails] = useState(false)
+  const [likes, setLikes] = useState(blog.likes)
   console.log(blog)
 
   const toggleDetails = () => {
     setShowDetails(details => !details)
   }
+
+  const handleUpdate = () => {
+
+    const updatedBlog = {
+      ...blog,
+      likes: likes + 1
+    }
+
+    blogService.update(blog.id, updatedBlog)
+      .then(returnedBlog => {
+        setLikes(returnedBlog.likes)
+      })
+      .catch(error => {
+        console.error("Error updating blog", error)
+      })
+  }
+
   return (
     <div className="blog">
       {blog.title} {blog.author}
@@ -17,7 +36,9 @@ const Blog = ({ blog }) => {
       {showDetails && (
         <div >
           <p>Url: {blog.url}</p>
-          <p>Likes: {blog.likes}</p>
+          <p>Likes: {likes} 
+            <button onClick={handleUpdate}> Like </button>
+          </p>
           <p>Author: {blog.author}</p>
         </div>
       )}
