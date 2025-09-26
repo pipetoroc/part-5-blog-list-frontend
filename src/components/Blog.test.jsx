c884c47484afd3a68eb68de094aab5e7d5c462da
pipetoroc <ftorocastilla@gmail.com>
2025-09-26 14:40:24 +0100

    exercise 5.14 testing button controller to show details(likes and url)

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders content', () => {
@@ -27,4 +28,28 @@ test('does not render likes', () => {
 
   const element = screen.queryByText('Likes: 10')
   expect(element).toBeNull()
+})
+
+test('clicking the "View" button shows blog details', async() => {
+  const blog = {
+    title: 'Testing button',
+    author: 'Laura Candido',
+    url: 'http://example.com',
+    likes: 5,
+    user: { username: 'laura' }
+  }
+
+  render(<Blog
+    blog={blog}
+    user={{ username: 'Laura' }}
+    onDelete={() => {}}
+  />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('View')
+  await user.click(button)
+
+  expect(screen.getByText(/Url:/i)).toBeInTheDocument()
+  expect(screen.getByText(/Likes:/i)).toBeInTheDocument()
+  expect(screen.getByText(/Author:/i)).toBeInTheDocument()
 })
