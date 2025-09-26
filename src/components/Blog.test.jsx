aaadc52387a5b5dfc2981d0389d59138e7d52a29
pipetoroc <ftorocastilla@gmail.com>
2025-09-26 10:41:48 +0100

    add test not render likes

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -9,9 +9,22 @@ test('renders content', () => {
 
   render(<Blog blog={blog} />)
 
-  const titleElement = screen.getByText(/This is a test in the frontend using @testing-library\/react/i)
+  const titleElement = screen.getByText('This is a test in the frontend using @testing-library', { exact: false })
   expect(titleElement).toBeDefined()
 
-  const authorElement = screen.getByText(/Fullstack open/i)
+  const authorElement = screen.getByText('Fullstack open', { exact: false })
   expect(authorElement).toBeDefined()
+})
+
+test('does not render likes', () => {
+  const blog = {
+    title: 'Blog with no likes',
+    author: 'Felipe',
+    likes: 10
+  }
+
+  render(<Blog blog={blog}/>)
+
+  const element = screen.queryByText('Likes: 10')
+  expect(element).toBeNull()
 })
