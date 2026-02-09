1aef4d7a311c472c92e611d43e0651b5092be02a
pipetoroc <ftorocastilla@gmail.com>
2026-02-09 15:45:57 -0500

    sort unmutable function

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
 import Notification  from './components/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
--- src/components/ListOfBlogs.jsx
+++ src/components/ListOfBlogs.jsx
@@ -4,14 +4,15 @@ export const ListOfBlogs =  ({ blogs, user, setBlogs }) => {
 
   return (
     <>
-      {blogs.sort((a, b) => a.likes - b.likes).map(blog => (
-        <Blog
-          key={blog.id}
-          blog={blog}
-          user={user}
-          onDelete={(id) => setBlogs(blogs.filter(b => b.id !== id))}
-        />)
-      )}
+      {[...blogs]
+        .sort((a, b) => a.likes - b.likes).map(blog => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            user={user}
+            onDelete={(id) => setBlogs(blogs.filter(b => b.id !== id))}
+          />)
+        )}
     </>
   )
 }
